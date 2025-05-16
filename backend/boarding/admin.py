3e2a57f295083276dedc986e799536d7c635f2cb
ananthakrishn04 <uiananthu1234@gmail.com>
2025-05-16 17:03:30 +0530

    feat(api) : add admin access to offboarding/onboarding table data

--- backend/boarding/admin.py
+++ backend/boarding/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Process, ProcessTemplate, Task, TaskTemplate, Equipment, EquipmentAssignment
 # Register your models here.
+
+
+admin.site.register(Process)
+admin.site.register(ProcessTemplate)
+admin.site.register(Task)
+admin.site.register(TaskTemplate)
+admin.site.register(Equipment)
+admin.site.register(EquipmentAssignment)
