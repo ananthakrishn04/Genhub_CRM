58aa5c7b0e29aae0ec629cd31a3da9bf8e666cc8
ananthakrishn04 <uiananthu1234@gmail.com>
2025-07-10 18:09:06 +0530

    update migrations

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -100,7 +100,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=config('DATABASE_URL', default='sqlite:///db.sqlite3')
+        default=config('DATABASE_URL')
     )
 }
 
