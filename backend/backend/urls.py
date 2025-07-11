2faf74b1472f815acbf1c5ad68262c9e70423a14
ananthakrishn04 <uiananthu1234@gmail.com>
2025-07-11 09:46:05 +0530

    readd debug rule for serving media files

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -38,5 +38,6 @@ urlpatterns = [
     path('api/analytics/', include('analytics.urls'))
 ]
 
-# Serve media files in production
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
